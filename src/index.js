d2f9704ee1d4ca7ac7833b8eb2c9fcbe7e1cf5eb
Sulisulisuli <101421381+Sulisulisuli@users.noreply.github.com>
2023-11-24 12:41:03 +0100

    test1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 window.Webflow ||= [];
 window.Webflow.push(() => {
   // Pisz kod między tymi nawiasami tak zaleca finsweet XD
-  console.log(myAge);
-  console.log('Siemano Suli!!!!');
+  console.log('Siemano Suli ziom!!!!');
 });
